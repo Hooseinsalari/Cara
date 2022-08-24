7a47de3184fc27247be7ac634865d454a0982d87
Hooseinsalari <hossein.salari002@gmail.com>
2022-08-24 21:56:21 +0430

    create footer

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 // components
 import Navbar from './components/shared/Navbar';
 import HomePage from './pages/HomePage';
+import Footer from './components/shared/Footer';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <HomePage />
+      <Footer />
     </div>
   );
 }
