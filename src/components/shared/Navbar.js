6d0c3dfb87b69ea7a4dfdd1d680c3ef605287070
Hooseinsalari <hossein.salari002@gmail.com>
2022-08-30 15:51:05 +0430

    add few styles to navbar

--- src/components/shared/Navbar.js
+++ src/components/shared/Navbar.js
@@ -1,8 +1,5 @@
-import React, { useContext, useState } from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
-
-// context
-import { cartContext } from "../../context/CartContextProvider";
 
 // styles
 import styles from "./Navbar.module.css";
@@ -11,12 +8,12 @@ import styles from "./Navbar.module.css";
 import logo from "../../img/logo.png";
 
 // icons
-import { FaBars, FaTimes } from "react-icons/fa";
+import { FaBars } from "react-icons/fa";
 import { BsHandbag } from "react-icons/bs";
+import { IoMdClose } from "react-icons/io";
 
 const Navbar = () => {
   const [toggle, setToggle] = useState(false);
-  const {state, dispath} = useContext(cartContext)
 
   const menuHandler = () => {
     setToggle((prevState) => !prevState);
@@ -24,11 +21,17 @@ const Navbar = () => {
 
   return (
     <nav className={styles.nav}>
-      <div className={styles.nav__logo}>
+      <NavLink to="/" className={styles.nav__logo}>
         <img src={logo} alt="logo" />
-      </div>
+      </NavLink>
 
-      <div className={styles.nav__right}>
+      <div
+        className={
+          toggle
+            ? [styles["nav__right"], styles["nav__right-active"]].join(" ")
+            : styles.nav__right
+        }
+      >
         <div onClick={menuHandler} className={styles.nav__toggle}>
           <FaBars />
         </div>
@@ -41,11 +44,12 @@ const Navbar = () => {
           }
         >
           <div onClick={menuHandler} className={styles.nav__close}>
-            <FaTimes />
+            <IoMdClose />
           </div>
           <ul className={styles.nav__list}>
             <li className={styles.nav__item}>
               <NavLink
+                onClick={() => setToggle(false)} //for close menu
                 className={({ isActive }) =>
                   isActive
                     ? [styles["nav__link"], styles["nav__link-active"]].join(
@@ -60,6 +64,7 @@ const Navbar = () => {
             </li>
             <li className={styles.nav__item}>
               <NavLink
+                onClick={() => setToggle(false)} //for close menu
                 className={({ isActive }) =>
                   isActive
                     ? [styles["nav__link"], styles["nav__link-active"]].join(
@@ -74,6 +79,7 @@ const Navbar = () => {
             </li>
             <li className={styles.nav__item}>
               <NavLink
+                onClick={() => setToggle(false)} //for close menu
                 className={({ isActive }) =>
                   isActive
                     ? [styles["nav__link"], styles["nav__link-active"]].join(
@@ -88,6 +94,7 @@ const Navbar = () => {
             </li>
             <li className={styles.nav__item}>
               <NavLink
+                onClick={() => setToggle(false)} //for close menu
                 className={({ isActive }) =>
                   isActive
                     ? [styles["nav__link"], styles["nav__link-active"]].join(
@@ -103,7 +110,15 @@ const Navbar = () => {
           </ul>
         </div>
 
-        <NavLink to="/cart" className={({isActive}) => isActive ? [styles['nav__cart'],styles['nav__cart-active']].join(' ') : styles.nav__cart}>
+        <NavLink
+          onClick={() => setToggle(false)} //for close menu
+          to="/cart"
+          className={({ isActive }) =>
+            isActive
+              ? [styles["nav__cart"], styles["nav__cart-active"]].join(" ")
+              : styles.nav__cart
+          }
+        >
           <BsHandbag />
         </NavLink>
 
